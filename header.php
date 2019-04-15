a1633d80ccd0f584011ba0ccaccc85abd7f62af0
Alex Hillman <alex@weknowhtml.com>
2019-04-15 16:06:39 -0400

    remove all header stuff

--- header.php
+++ header.php
@@ -13,18 +13,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-	<!-- RightMessage -->
-	<script type="text/javascript">
-	(function(p, a, n, d, o, b) {
-	    o = n.createElement('script'); o.type = 'text/javascript'; o.async = true; o.src = 'https://tag.rightmessage.com/'+p+'.js';
-	    b = n.getElementsByTagName('script')[0]; b.parentNode.insertBefore(o, b);
-	    d = function(h, u, i) { var o = n.createElement('style'); o.id = 'rmcloak'+i; o.type = 'text/css';
-	        o.appendChild(n.createTextNode('.rmcloak'+h+'{visibility:hidden}.rmcloak'+u+'{display:none}'));
-	        b.parentNode.insertBefore(o, b); return o; }; o = d('', '-hidden', ''); d('-stay-invisible', '-stay-hidden', '-stay');
-	    setTimeout(function() { o.parentNode && o.parentNode.removeChild(o); }, a);
-	})('1440655857', 2500, document);
-	</script>
-	<script async id="_ck_407191" src="https://forms.convertkit.com/407191?v=7"></script>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
@@ -33,7 +21,6 @@
 	<meta name="google-site-verification" content="6m_5qat17m1TlRCBEdqDnUdd-aj2pE6P5XlgPYmYOxc" />
 
 	<?php wp_head(); ?>
-
 </head>
 
 <body <?php body_class(); ?>>
